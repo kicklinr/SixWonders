b132b4b93df3aa7a9cc50315dfcee1dc03b7859f
Emily Coates <emily.agathen@sbcglobal.net>
2015-09-12 22:08:14 -0500

    Added notify web service

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var pg = require('pg-query');
 
+// Twilio Credentials 
+var accountSid = 'AC36a5b1edc49a8f0542cdd602ab925a83'; 
+var authToken = '62eafd50007bdc5cd48ba1ee4547a200'; 
+
+//require the Twilio module and create a REST client 
+var twilio = require('twilio')(accountSid, authToken); 
+
 pg.connectionParameters = "postgres://iltnencxwnrati:69VlHFO9ejuhDno8X21odCthhZ@ec2-107-21-105-116.compute-1.amazonaws.com:5432/d3vatqm4sct45j?ssl=true";
 
 var routes = require('./routes/index');
 var search = require('./routes/search');
 var feedback = require('./routes/feedback');
+var sms = require('./routes/sms');
 var app = express();
 
 // view engine setup
@@ -28,11 +36,16 @@ app.use(function(req,res,next){
 	req.pgQuery = pg;
 	next();
 });
+app.use(function(req,res,next){
+  req.twilio = twilio;
+  next();
+});
 
 app.use('/', routes);
 app.use('/citations', routes);
 app.use('/search', search);
 app.use('/feedback', feedback);
+app.use('/sms', sms);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
